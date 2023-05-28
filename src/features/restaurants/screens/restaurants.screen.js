36f1fe47cc26e02129b5d24e22e44b71b2d7e8c5
Arzan <khanazzu994>
2023-05-28 12:39:35 +0530

    commit on mealstogo

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+export const RestaurantsScreen = () =>(
+    
+    <><SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+            <Searchbar />
+        </View>
+        <View style={styles.list}>
+            <Text>list</Text>
+        </View>
+    </SafeAreaView><ExpoStatusBar style="auto" /></>
+ 
+);
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight,
+    },
+    search: {
+      padding: 16,
+      backgroundColor: "white",
+    },
+    list: {
+      flex: 1,
+      padding: 16,
+      backgroundColor: "blue",
+    },
+  });
+  
