b4842a452dc156368a79d400ee72d62406e1d7a6
Arzan <khanazzu994>
2023-06-05 20:10:02 +0530

    commit on master

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -6,21 +6,39 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
 background-color: ${(props) => props.theme.colors.bg.primary};
-`; 
+`;
 
 const RestaurantCardCover = styled(Card.Cover)`
 padding: ${(props) => props.theme.space[3]};
 background-color: ${(props) => props.theme.colors.bg.primary};
 `;
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+
+
+const Info = styled.View`
+padding: ${(props) => props.theme.space[3]};
+`;
 
 const Title = styled(Text)`
-padding: ${(props) => props.theme.space[3]};
+
 color: ${(props) => props.theme.colors.ui.primary};
 font-size: ${(props)=>props.theme.fontSizes.title}
 `;
+
+const Address = styled(Text)`
+font-size: ${(props)=>props.theme.fontSizes.caption}
+`;
+
 
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -35,12 +53,21 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       rating = 4,
       isClosedTemporarily,
     } = restaurant;
-  
+
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
+
     return (
       <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+        <Info>
         <Title>{name}</Title>
+        <Rating>
+        {ratingArray.map(() => (
+          <SvgXml xml={star} width={20} height={20} />
+        ))}
+      </Rating>
+        <Address>{address}</Address>
+        </Info>
       </RestaurantCard>
     );
     };
-   
